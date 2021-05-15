0f7dac88d7eb6af09814ad6bbeb7732a27f0f372
Sumeyye Uzgor <summeyyeoz@gmail.com>
2021-05-15 13:25:32 +0300

    camera is setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,247 @@
 <template>
-  <hello-world />
+  <v-row justify="center" class="mt-5">
+      <v-col cols="12" align="center">
+        <v-row justify="center">
+            <h1>
+                <img src="../assets/camera2.svg" width="30px" height="auto" class="mr-1"/>
+                Catch My Photo
+            </h1>
+        </v-row>
+        <v-row justify="center" class="mt-5">
+            <v-col cols="12" align="center">
+                  <v-row class="camera-button" justify="center">
+                    <v-btn type="button" :class="{ 'blue darken-4' : !isCameraOpen, 'red darken-4' : isCameraOpen}" @click="toggleCamera">
+                        <span v-if="!isCameraOpen">Open Camera</span>
+                        <span v-else>Close Camera</span>
+                    </v-btn>
+                </v-row>
+  
+                <v-row v-show="isCameraOpen && isLoading" class="camera-loading" justify="center">
+                    <ul class="loader-circle">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </v-row>
+            </v-col>
+        </v-row>
+        <v-row justify="center">
+            <v-col cols="12" align="center">
+                <v-row v-if="isCameraOpen" v-show="!isLoading" class="camera-box" :class="{ 'flash' : isShotPhoto }" justify="center">
+                    <div class="camera-shutter" :class="{'flash' : isShotPhoto}"></div>
+                    <video v-show="!isPhotoTaken" ref="camera" :width="450" :height="337.5" autoplay></video>
+                    <canvas v-show="isPhotoTaken" id="photoTaken" ref="canvas" :width="450" :height="337.5"></canvas>
+                </v-row>
+    
+                <v-row v-if="isCameraOpen && !isLoading" justify="center" class="mt-7">
+                    <button type="button" class="button" @click="takePhoto">
+                        <img src="../assets/camera2.svg" width="50px" height="auto" class="mr-1"/>
+                    </button>
+                </v-row>
+  
+                <v-row v-if="isPhotoTaken && isCameraOpen" class="camera-download" justify="center">
+                    <a id="downloadPhoto" download="my-photo.jpg" class="button" role="button" @click="downloadImage">
+                    Download
+                    </a>
+                </v-row>
+            </v-col>
+        </v-row>
+      </v-col>
+  </v-row>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+    export default {
+        name: 'Home',
+        components: {
+        },
 
-  export default {
-    name: 'Home',
+        data() {
+            return {
+                isCameraOpen: false,
+                isPhotoTaken: false,
+                isShotPhoto: false,
+                isLoading: false,
+                link: '#'
+            }
+        },
+        
+        methods: {
+            toggleCamera() {
+                    if(this.isCameraOpen) {
+                        this.isCameraOpen = false;
+                        this.isPhotoTaken = false;
+                        this.isShotPhoto = false;
+                        this.stopCameraStream();
+                    } else {
+                        this.isCameraOpen = true;
+                        this.createCameraElement();
+                    }
+            },
+                
+            createCameraElement() {
+                this.isLoading = true;
+                    
+                const constraints = (window.constraints = {
+                    audio: false,
+                    video: true
+                });
 
-    components: {
-      HelloWorld,
-    },
-  }
+
+                navigator.mediaDevices
+                    .getUserMedia(constraints)
+                    .then(stream => {
+                            this.isLoading = false;
+                            this.$refs.camera.srcObject = stream;
+                    })
+                    .catch(() => {
+                        this.isLoading = false;
+                        alert("May the browser didn't support or there is some errors.");
+                    });
+            },
+    
+            stopCameraStream() {
+                let tracks = this.$refs.camera.srcObject.getTracks();
+                tracks.forEach(track => {
+                    track.stop();
+                });
+            },
+                    
+            takePhoto() {
+                if(!this.isPhotoTaken) {
+                    this.isShotPhoto = true;
+
+                    const FLASH_TIMEOUT = 50;
+
+                    setTimeout(() => {
+                    this.isShotPhoto = false;
+                    }, FLASH_TIMEOUT);
+                }
+                    
+                this.isPhotoTaken = !this.isPhotoTaken;
+                    
+                const context = this.$refs.canvas.getContext('2d');
+                context.drawImage(this.$refs.camera, 0, 0, 450, 337.5);
+            },
+                    
+            downloadImage() {
+                const download = document.getElementById("downloadPhoto");
+                const canvas = document.getElementById("photoTaken").toDataURL("image/jpeg")
+                .replace("image/jpeg", "image/octet-stream");
+                download.setAttribute("href", canvas);
+            }
+        },
+    }
 </script>
+<style scoped>
+
+.web-camera-container {
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 500px;
+}
+  
+  .camera-button {
+    margin-bottom: 2rem;
+  }
+  
+  .camera-box .camera-shutter {    
+      opacity: 0;
+      width: 450px;
+      height: 337.5px;
+      background-color: #fff;
+      position: absolute;
+  }
+  .camera-box .camera-shutter .flash{  
+      opacity: 1;
+    }  
+  
+  .camera-shoot {
+    margin: 1rem 0;
+
+  }
+  .camera-shoot button{
+
+      height: 60px;
+      width: 60px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: 100%;
+  }
+  
+  .camera-shoot button img{
+      height: 35px;
+        object-fit: cover;
+  }
+
+  .camera-loading {
+    overflow: hidden;
+    height: 100%;
+    position: absolute;
+    width: 100%;
+    min-height: 150px;
+    margin: 3rem 0 0 -1.2rem;
+    
+  }
+    .camera-loading ul {
+      height: 100%;
+      position: absolute;
+      width: 100%;
+      z-index: 999999;
+      margin: 0;
+    }
+    
+    .camera-loading .loader-circle {
+      display: block;
+      height: 14px;
+      margin: 0 auto;
+      top: 50%;
+      left: 100%;
+      transform: translateY(-50%);
+      transform: translateX(-50%);
+      position: absolute;
+      width: 100%;
+      padding: 0;
+    }
+    .camera-loading .loader-circle li {
+        display: block;
+        float: left;
+        width: 10px;
+        height: 10px;
+        line-height: 10px;
+        padding: 0;
+        position: relative;
+        margin: 0 0 0 4px;
+        background: #999;
+        animation: preload 1s infinite;
+        top: -50%;
+        border-radius: 100%;
+    }   
+    .camera-loading .loader-circle li:nth-child(2)  {
+          animation-delay: .2s;
+    }
+    .camera-loading .loader-circle li:nth-child(3)  {
+          animation-delay: .4s;
+    }
+
+    /* .web-camera-container @keyframes preload {
+        0% {
+        opacity: 1
+        }
+        50% {
+        opacity: .4
+        }
+        100% {
+        opacity: 1
+        }
+    } */
+
+</style>
