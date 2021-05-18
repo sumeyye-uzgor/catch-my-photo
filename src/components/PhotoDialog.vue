ede47ecfc695f8611175d4cb711478b543c20381
Sumeyye Uzgor <summeyyeoz@gmail.com>
2021-05-18 18:46:44 +0300

    photo dialog is updated, canvas square is added

--- src/components/PhotoDialog.vue
+++ src/components/PhotoDialog.vue
@@ -1,62 +1,113 @@
 <template>
-      <v-dialog
-        transition="scale-transition"
-        max-width="600"
-        :value="this.$store.state.photoShoot"
-          >            
-        <v-card color="#05386b" dark>
+    <v-dialog
+    transition="scale-transition"
+    max-width="900"
+    :value="this.$store.state.photoShoot"
+    >
+        <v-card color="#05386b" dark class="ma-0 pa-0">
             <v-container>
                 <v-row justify="center">
-                    <v-col cols="6" justify="center">
-                        <v-img :src="this.$store.state.photoShoot.src"/>
+                    <v-col cols="6" justify="center" class="canvas-wrapper">
+                        <v-img :src="this.$store.state.photoShoot.src" width="250" height="250"/>
+                        <canvas ref="myCanvas" class="canvas-overlay"></canvas>
                     </v-col>
                     <v-col cols="6" justify="center">
                         <v-text-field
                         label="Name"
                         v-model="name"
-                        placeholder="Placeholder"
+                        placeholder="Name"
                         outlined />
                         <v-text-field
                         label="Location"
                         v-model="location"
-                        placeholder="Placeholder"
+                        placeholder="Location"
                         outlined />
                         <v-text-field
                         label="Date"
                         disabled
                         :value="date"
-                        placeholder="Placeholder"
                         outlined />
+                        <v-row>
+                            <v-col cols="6">
+                                <v-text-field
+                                label="Start at (X)"
+                                v-model="coordinates[0]"
+                                outlined />
+                            </v-col>
+                            <v-col cols="6">
+                                <v-text-field
+                                label="Start at (Y)"
+                                v-model="coordinates[1]"
+                                outlined />
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="6">
+                                <v-text-field
+                                label="Width"
+                                v-model="coordinates[2]"
+                                outlined />
+                            </v-col>
+                            <v-col cols="6">
+                                <v-text-field
+                                label="Height"
+                                v-model="coordinates[3]"
+                                outlined />
+                            </v-col>
+                        </v-row>
                     </v-col>
                 </v-row>
             </v-container>
             <v-card-actions class="justify-end">
-              <v-btn
-                text
-                @click="closeDialog"
-              >Close</v-btn>
-                <v-btn
-                text
-                @click="savePhoto"
-              >Save Photo</v-btn>
-            </v-card-actions>
-          </v-card>
-      </v-dialog>
+                    <v-btn text @click="drawSquare">
+                        Draw Square
+                    </v-btn>
+                    <v-btn
+                        text
+                        @click="closeDialog"
+                    >Close</v-btn>
+                        <v-btn
+                        text
+                        @click="savePhoto"
+                    >Save Photo</v-btn>
+
+                </v-card-actions>
+            </v-card>
+    </v-dialog>
 
 </template>
 
 <script>
   export default {
     name: 'PhotoDialog',
+    // props:["isFromHome", "isFromPhotos"],
+    mounted(){
+        const imgRef = this.$refs['myCanvas']
+        let ctx = imgRef.getContext('2d')
+        this.canvas = ctx
+        this.canvas.lineWidth = "2"
+        this.canvas.beginPath()
+        this.canvas.rect(...this.coordinates)
+        this.canvas.stroke()
+        this.canvas.strokeStyle = 'red';
+    },
     data(){
         return{
             date: Date().toString(),
             location: '',
             name: '',
-            coordinates: [],
+            coordinates: [100, 100, 50, 50],
         }
     },
     methods:{
+        drawSquare(){
+            this.canvas.clearRect(0,0,250,250)
+            this.canvas.beginPath()
+            this.canvas.rect(...this.coordinates)
+            this.canvas.stroke()
+            this.canvas.strokeStyle = 'red';
+
+        },
         savePhoto(){
             try{
                 const photo = {
@@ -80,3 +131,19 @@
     },
   }
 </script>
+<style scoped>
+.canvas-overlay {
+  position: absolute !important;
+  top: 0 !important;
+  left: 0 !important;
+  right: 0 !important;
+  bottom: 0 !important;
+  pointer-events: none !important;
+  width: 100% !important;
+  height: 100 !important;
+}
+
+.canvas-wrapper {
+  position: relative !important;
+}
+</style>
--- src/views/Photos.vue
+++ src/views/Photos.vue
@@ -1,19 +1,20 @@
 <template>
-    <v-container  class="ma-0 pb-16 pt-10">
-        <v-row v-if="this.$store.state.photos" class="pl-10">
-            <v-col cols="4" v-for="(photo) in this.$store.state.photos" :key="photo.id">
+    <v-container grid-list class="ma-0 pb-16 pt-10">
+        <v-flex wrap align-center justify-center>
+        <v-row v-if="this.$store.state.photos" align="between">
+            <v-col justify="center" cols="4" v-for="(photo) in this.$store.state.photos" :key="photo.id">
                 <v-card
                     class="pa-10"
                     color="#05386b" dark>
                     <v-img :src="photo.imageUrl"/>
                     <v-card-text>
-                        <v-text-field disabled :value="photo.name" label="Name" outlined />
+                        <!-- <v-text-field disabled :value="photo.name" label="Name" outlined />
                         <v-text-field disabled :value="photo.location" label="Location" outlined />
                         <v-text-field disabled :value="photo.coordinates" label="Coordinates" outlined />
-                        <v-text-field disabled :value="photo.date" label="Dates" outlined />
+                        <v-text-field disabled :value="photo.date" label="Dates" outlined /> -->
                     </v-card-text>
                     <v-card-actions class="justify-end">
-                        <v-btn>Edit</v-btn>
+                        <!-- <v-btn>Edit</v-btn> -->
                         <v-btn>Details</v-btn>
                     </v-card-actions>
                 </v-card>
@@ -25,7 +26,8 @@
                 
             </v-col>
         </v-row>
- 
+    
+    </v-flex>
 
     </v-container>
 
